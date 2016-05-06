77d4b1f71cb85c1b2eb20f8f16c53856cd4e4793
Denis Mysenko <denis@mylocalbroker.com.au>
2016-05-06 15:57:08 +1000

    + Adding tests

---
+++ tests/PlainSqs/QueueTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Dusterio\PlainSqs\Tests;
+use Aws\Sqs\SqsClient;
+use Dusterio\PlainSqs\Jobs\DispatcherJob;
+use Dusterio\PlainSqs\Sqs\Queue;
+
+/**
+ * Class QueueTest
+ * @package Dusterio\PlainSqs\Tests
+ */
+class QueueTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function class_named_is_derived_from_queue_name()
+    {
+
+        $content = [
+            'test' => 'test'
+        ];
+
+        $job = new DispatcherJob($content);
+
+        $queue = $this->getMockBuilder(Queue::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $method = new \ReflectionMethod(
+            'Dusterio\PlainSqs\Sqs\Queue', 'createPayload'
+        );
+
+        $method->setAccessible(true);
+
+        //$response = $method->invokeArgs($queue, [$job]);
+    }
+}
